5f193192bea5966abf8340b5f435eb51d53d6eee
shubham-mendhare <shubhammendhare9@gmail.com>
2023-05-02 11:59:48 +0530

    Base and Config pkg moved to main folder

--- src/main/java/smtech/utility/Utility.java
+++ src/main/java/smtech/utility/Utility.java
@@ -8,9 +8,9 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Utility {
-	public WebDriver driver;
-	public WebDriverWait wait;
-	public Utility(WebDriver driver) {
+	 WebDriver driver;
+	 WebDriverWait wait;
+	 protected Utility(WebDriver driver) {
 		this.driver=driver;
 	}
 	
